f96e7a50a895c072303250b8a1aa1b81a89daace
Gela  Kharebashvili <gelakhareba@gmail.com>
2023-01-17 18:18:07 +0100

    added git_test class

---
+++ src/test/java/com/cydeo/tests/git_practice/git_test.java
@@ -0,0 +1,11 @@
+package com.cydeo.tests.git_practice;
+
+import org.testng.annotations.Test;
+
+public class git_test {
+
+    @Test
+    public void test1(){
+        System.out.println("test1");
+    }
+}
