1c0c27c4034b6f0808c77e476dfcbccc6cb5d9e1
Gela  Kharebashvili <gelakhareba@gmail.com>
2023-01-26 17:41:15 +0100

    many tasks

---
+++ src/test/java/com/cydeo/tests/day11_driver_close_actions_jsexecutor_practices/ActionsPractice.java
@@ -0,0 +1,17 @@
+package com.cydeo.tests.day11_driver_close_actions_jsexecutor_practices;
+
+import org.testng.annotations.Test;
+
+public class ActionsPractice {
+
+    @Test void task_4_and_5_test(){
+
+        //TC #4: Scroll practice
+        //1- Open a Chrome browser
+        //2- Go to: https://practice.cydeo.com/
+        //3- Scroll down to “Powered by CYDEO”
+        //4- Scroll using Actions class “moveTo(element)” method
+    }
+
+
+}
